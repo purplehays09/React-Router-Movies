963c2b3750d92c2eee9f568f4da5409d112fc4ea
purplehays09 <purplehays527@gmail.com>
2020-08-17 13:08:45 -0600

    I got it working so far but I have had to restart the server like 5 times I dont know why this happens but Ill see if I can figure it out

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link, Route, Switch } from 'react-router-dom'
 
 import SavedList from './Movies/SavedList';
 
